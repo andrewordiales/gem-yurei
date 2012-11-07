025cca173a2a2250fa17823ffc1d56893d973432
Andy <andrew@friendster.com>
2012-11-07 16:40:35 +0800

    Added: Support for 64-bit Ubuntu Lucid (10.04)

--- lib/yurei.rb
+++ lib/yurei.rb
@@ -4,13 +4,22 @@ require 'fileutils'
 module Yurei
 
   class << self
-    
+
     def phantomjs_path
-      bin_path = File.join vendor_path, 'phantomjs', '1.7.0', 'macosx', 'bin', 'phantomjs' 
+      bin_path = File.join vendor_path, 'phantomjs', '1.7.0'
+
+      case platform
+        when 'linux' then
+          bin_path = File.join bin_path, 'linux-x86_64', 'bin', 'phantomjs'
+        when 'darwin' then
+          bin_path = File.join bin_path, 'macosx', 'bin', 'phantomjs'
+      end
+
+      bin_path
     end
 
     def casperjs_path
-      cj_path = File.join vendor_path, 'casperjs'
+      File.join vendor_path, 'casperjs'
     end
 
     def vendor_path
@@ -19,6 +28,14 @@ module Yurei
 
     def base_path
       File.dirname(File.dirname(File.expand_path(__FILE__)))
+    end
+
+    def platform
+      case RbConfig::CONFIG['host_os'].downcase
+        when /linux/  then 'linux'
+        when /darwin/ then 'darwin'
+        else 'unsupported'
+      end
     end
 
   end
